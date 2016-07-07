a4a91d952ad42466e945cc97a425eb8b68d1bd51
Guruprasad Kulkarni <guru@linux.com>
2016-07-07 19:43:12 +0530

    Added First Page and updated some packages. No web.xml Yay!

---
+++ src/main/java/com/linux/home/HomePage.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2016 guru.a.kulkarni
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.linux.home;
+
+import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.model.ResourceModel;
+
+/**
+ *
+ * @author guru.a.kulkarni
+ */
+public class HomePage extends WebPage {
+
+    public HomePage() {
+        add(new Label("helloWicket", new ResourceModel("helloWicket")));
+    }
+
+    
+    
+    
+}
