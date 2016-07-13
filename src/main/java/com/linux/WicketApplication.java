b270fef95b758553acbc874d89fbea9ca20ca620
Guruprasad Kulkarni <guru@linux.com>
2016-07-13 09:38:10 +0530

    Starting to add Wicket Tabs

--- src/main/java/com/linux/WicketApplication.java
+++ src/main/java/com/linux/WicketApplication.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016 guru.a.kulkarni
+ * Copyright (C) 2016 comdotlinux <guru@linux.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
--- src/main/java/com/linux/home/HomePage.java
+++ src/main/java/com/linux/home/HomePage.java
@@ -23,6 +23,7 @@ import java.util.ArrayList;
 import java.util.List;
 import org.apache.wicket.extensions.ajax.markup.html.tabs.AjaxTabbedPanel;
 import org.apache.wicket.extensions.markup.html.tabs.ITab;
+import org.apache.wicket.markup.head.CssReferenceHeaderItem;
 import org.apache.wicket.markup.head.IHeaderResponse;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.WebPage;
@@ -30,43 +31,37 @@ import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.model.ResourceModel;
+import org.apache.wicket.request.resource.PackageResourceReference;
 import static org.apache.wicket.util.lang.Args.notNull;
 
 /**
- *
- * @author guru.a.kulkarni
+ * Home Page and entry to the application
+ * @author comdotlinux <guru@linux.com>
  */
 public class HomePage extends WebPage {
-    
 
     @Override
     protected void onInitialize() {
         super.onInitialize();
         List<Tab> tabs = new ArrayList<>();
-        
-//        IModel<Boolean> projectNameTabVisible = new Model<>(true);
-//        Tab projectName = new Tab(new ResourceModel("projectNameTabTitle"), projectNameTabVisible);
-//        tabs.add(projectName);
-        
+
         IModel<Boolean> homeTabVisible = new Model<>(true);
         Tab home = new Tab(new ResourceModel("homeTabTitle"), homeTabVisible);
         tabs.add(home);
-        
+
         IModel<Boolean> aboutTabVisible = new Model<>(true);
         Tab about = new Tab(new ResourceModel("aboutTabTitle"), aboutTabVisible);
         tabs.add(about);
-        
+
         IModel<Boolean> contactTabVisible = new Model<>(true);
         Tab contact = new Tab(new ResourceModel("contactTabTitle"), contactTabVisible);
         tabs.add(contact);
-        
-        add(new AjaxTabbedPanel<Tab>("tabPanel", tabs){
+
+        add(new AjaxTabbedPanel<Tab>("tabPanel", tabs) {
             @Override
             protected String getSelectedTabCssClass() {
                 return "active";
             }
-            
-            get
         });
         add(new Label("helloWicket", new ResourceModel("helloWicket")));
     }
@@ -76,14 +71,15 @@ public class HomePage extends WebPage {
         response.render(CSS_BOOTSTRAP);
         response.render(CSS_BOOTSTRAP_THEME);
         response.render(JS_BOOTSTRAP);
+        response.render(CssReferenceHeaderItem.forReference(new PackageResourceReference(HomePage.class, "./custom.css")));
     }
-    
-    private class Tab implements ITab{
+
+    private class Tab implements ITab {
 
         private final IModel<String> title;
         private WebMarkupContainer panel;
         private final IModel<Boolean> isVisible;
-        
+
         Tab(final IModel<String> title, final IModel<Boolean> isVisible) {
             this.title = notNull(title, "title");
             this.isVisible = notNull(isVisible, "isVisible");
@@ -96,8 +92,9 @@ public class HomePage extends WebPage {
 
         @Override
         public WebMarkupContainer getPanel(String containerId) {
-            if( panel == null ) {
+            if (panel == null) {
                 panel = new WebMarkupContainer(containerId);
+                panel.setOutputMarkupPlaceholderTag(true);
             }
             return panel;
         }
@@ -106,6 +103,6 @@ public class HomePage extends WebPage {
         public boolean isVisible() {
             return null != isVisible && null != isVisible.getObject() && isVisible.getObject();
         }
-        
+
     }
 }
